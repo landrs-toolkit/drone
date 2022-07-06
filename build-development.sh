26f184cacc8c17aad5ef1b1b1674974db4adf96f
Charles Vardeman <charles.vardeman@gmail.com>
2022-07-05 20:26:14 -0400

    Adding development version of ontology

--- build-development.sh
+++ build-development.sh
@@ -11,25 +11,21 @@ SHACL=drone.shacl
 
 VERSION=` grep -i versionInfo modules/core/metadata.ttl | sed 's/[^"]*"\([^"]*\).*/\1/'`
 
-if [ -f "./development/${ONTOLOGY}.ttl" ]; then
-    rm ./development/${ONTOLOGY}.ttl
-fi
-
-if [ -f "./development/${SHACL}" ]; then
-    rm  ./development/${SHACL}.ttl
+if [ -f "./development/ontology/merged.ttl" ]; then
+    rm ./development/ontology/merged.ttl
 fi
 
 echo "Merging Ontology"
-$mergecmd merge $files -f ttl -o ./development
-mv ./development/merged.ttl ./development/${ONTOLOGY}.ttl
-# echo "Generating HTML Ontology Documentation"
-# $pylodecmd ./development/${ONTOLOGY}.ttl -o ./development/${ONTOLOGY}.html
+$mergecmd merge $files -f ttl -o ./development/ontology
 
-echo "Merging Shapes"
-$mergecmd merge $shapefiles -f ttl -o ./development
-mv ./development/merged.ttl ./development/${SHACL}.ttl
+# echo "Merging Shapes"
+# $mergecmd merge $shapefiles -f ttl -o ./development/raw
 #echo "Generating HTML SHACL Shapes Documentation"
 # $pylodecmd ./development/${SHACL}.ttl -o ./development/${SHACL}.html
 
 echo "Generating HTML Ontology Documentation"
-docker run -ti --rm   -v `pwd`/development:/usr/local/widoco/in   -v `pwd`/development:/usr/local/widoco/out   dgarijo/widoco -ontFile in/drone.ttl -outFolder out -rewriteAll -webVowl
+docker run -ti --rm   -v `pwd`/development/ontology:/usr/local/widoco/in   -v `pwd`/development/ontology:/usr/local/widoco/out/doc   dgarijo/widoco -ontFile in/merged.ttl -outFolder out -rewriteAll -webVowl 
+# Clean up temp merged file
+if [ -f "./development/ontology/merged.ttl" ]; then
+    rm ./development/ontology/merged.ttl
+fi
